71da06d65d094d43ad5f1e657f861317f33c28d3
tamir3232 <tamirrasydi@gmail.com>
2022-06-06 12:29:47 +0700

    menambah file dosen

---
+++ dosen.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>DATA DOSEN</h1>
+    <UL>
+        <li>pak agus </li>
+        <li>wow</li>
+    </UL>
+    
+</body>
+</html>
